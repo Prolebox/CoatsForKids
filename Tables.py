44b02c1c46d1593102b4ff4827576a5eb4685d9f
Prolebox <ethansuhr14@gmail.com>
2022-07-05 01:10:03 -0600

    Restructured SQL database. Tested data input and viewing

--- Tables.py
+++ Tables.py
@@ -32,42 +32,31 @@ def Create():
                 glove_type check(glove_type in ('Boys','Girls','Mens','Womens')) not null,
                 glove_id integer primary key
             );
-            create table if not exists Parent (
-                parent_first text not null,
-                parent_last text not null,
-                parent_street text not null,
-                parent_city text not null,
-                parent_state text check(parent_state in ('Montana')) not null,
-                parent_zip text not null,
-                parent_phone integer not null,
-                parent_id integer primary key
-            );
-            create table if not exists Child (
+
+            create table if not exists Record (
                 child_first text not null,
                 child_last text not null,
-                child_school text not null,
                 child_age int(2) not null,
                 child_gender text check(child_gender in('Male','Female','Other')) not null,
-                child_id integer primary key,
-                child_parent_id integer not null,
-                    foreign key (child_parent_id) references Parent (parent_id)
+                child_school text not null,
+                parent_first text not null,
+                parent_last text not null,
+                parent_phone integer not null,
+                parent_street text not null,
+                parent_city text not null,
+                parent_zip text not null,
+                hat text,
+                coat text,
+                gloves text,
+                socks text,
+                boots text,
+                record_id integer primary key
           );
-            create table if not exists Helped (
-                h_hat_id integer,
-                h_coat_id integer,
-                h_glove_id integer,
-                h_sock_id integer,
-                h_boot_id integer,
-                h_parent_id integer not null,
-                h_date date not null,
-                    foreign key (h_hat_id) references Hat (hat_id),
-                    foreign key (h_coat_id) references Coat (coat_id),
-                    foreign key (h_glove_id) references Glove (glove_id),
-                    foreign key (h_sock_id) references Sock (sock_id),
-                    foreign key (h_boot_id) references Boot (boot_id),
-                    foreign key (h_parent_id) references Parent (parent_id)
-          );
+
         """)
+
+
+
     except:
         Print("An error has occured.")
         Quit()
